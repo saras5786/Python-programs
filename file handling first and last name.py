d8841c80e34e28183cda8af8c43f5f11c7150a12
saras5786 <saraschandra5786@gmail.com>
2026-04-19 10:51:00 +0530

    Add function to print full name from input

---
+++ file handling first and last name.py
@@ -0,0 +1,9 @@
+def print_full_name(first, last):
+    print(f"Hello {first} {last}! You just delved into python.")
+
+# Read input from STDIN
+first = input().strip()
+last = input().strip()
+
+# Process and print
+print_full_name(first, last)
